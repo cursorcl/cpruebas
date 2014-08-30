eb1cfc1607cc166bd9a84187d6d72a8f87610908
Eliecer Osorio Verdugo <cursor.cl@gmail.com>
2014-08-30 17:14:55 -0400

    Probando otra vez

--- cpruebas.pruebas/src/cl/eos/controller/PruebasController.java
+++ cpruebas.pruebas/src/cl/eos/controller/PruebasController.java
@@ -4,7 +4,10 @@ import cl.eos.imp.controller.AController;
 
 public class PruebasController extends AController {
 
+	private String name =  "Hola";
 	public PruebasController() {
 		// TODO Auto-generated constructor stub
 	}
+	
+	
 }
