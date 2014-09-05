3dfc22fc7223512c4c3318dbfad64eb96b2e403a
eosorio <cursor.cl@gmail.com>
2014-09-05 01:15:52 -0400

    Commit

---
+++ cpruebas.persistence/src/cl/eos/persistence/models/Nivel.java
@@ -0,0 +1,40 @@
+package cl.eos.persistence.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import cl.eos.interfaces.entity.IEntity;
+
+public class Nivel implements IEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public void setName(String name) {
+		this.name = name;
+
+	}
+
+	@Override
+	public boolean validate() {
+		return false;
+	}
+}
