7550cb019451b206f63a9180be4a6bb80f2b411a
eosorio <cursor.cl@gmail.com>
2014-09-12 02:37:10 -0300

    Alternativas y Respuestas

---
+++ cpruebas.pruebas/src/cl/eos/view/AlternativasView.java
@@ -0,0 +1,32 @@
+package cl.eos.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.layout.VBox;
+
+public class AlternativasView extends VBox{
+
+	@FXML
+	private Label lbl_1;
+	@FXML
+	private Label lbl_2;
+	@FXML
+	private Label lbl_3;
+	@FXML
+	private Label lbl_4;
+	@FXML
+	private Label lbl_5;
+	
+	@FXML
+	private RadioButton rb1A;
+	@FXML
+	private RadioButton rb1B;
+	@FXML
+	private RadioButton rb1C;
+	@FXML
+	private RadioButton rb1D;
+	@FXML
+	private RadioButton rb1E;
+
+}
