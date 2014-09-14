09f8cece68f159a2db66c14e0bab2867dffa172d
Eliecer Osorio Verdugo <cursor.cl@gmail.com>
2014-09-14 16:55:57 -0300

    Se egrega el WindowManager para poder incorporar nuevas ventanas al sistema.

--- cpruebas.main/src/cl/eos/MainController.java
+++ cpruebas.main/src/cl/eos/MainController.java
@@ -7,122 +7,127 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import cl.eos.imp.view.WindowManager;
 import cl.eos.interfaces.IActivator;
 import cl.eos.provider.persistence.PersistenceServiceFactory;
 
 public class MainController {
 
-	@FXML
-	private MenuContainer mnuPrincipal;
-	@FXML
-	private Menu mnuAdministrar;
-	@FXML
-	private MenuItem mnuAlumno;
-	@FXML
-	private MenuItem mnuCursos;
-	@FXML
-	private MenuItem mnuProfesores;
-	@FXML
-	private MenuItem mnuAsignaturas;
-	@FXML
-	private MenuItem mnuColegios;
-	@FXML
-	private MenuItem mnuEjesTematicos;
-	@FXML
-	private MenuItem mnuHabilidades;
-	@FXML
-	private MenuItem mnuHacerPrueba;
-	@FXML
-	private MenuItem mnuEvaluarPrueba;
-	@FXML
-	private MenuItem mnuItemGeneraBD;
-	@FXML
-	private StackPane pnlContainer;
+  @FXML
+  private MenuContainer mnuPrincipal;
+  @FXML
+  private Menu mnuAdministrar;
+  @FXML
+  private MenuItem mnuAlumno;
+  @FXML
+  private MenuItem mnuCursos;
+  @FXML
+  private MenuItem mnuProfesores;
+  @FXML
+  private MenuItem mnuAsignaturas;
+  @FXML
+  private MenuItem mnuColegios;
+  @FXML
+  private MenuItem mnuEjesTematicos;
+  @FXML
+  private MenuItem mnuHabilidades;
+  @FXML
+  private MenuItem mnuHacerPrueba;
+  @FXML
+  private MenuItem mnuEvaluarPrueba;
+  @FXML
+  private MenuItem mnuItemGeneraBD;
+  @FXML
+  private StackPane pnlContainer;
 
-	public MainController() {
-		super();
-	}
+  public MainController() {
+    super();
+  }
 
-	@FXML
-	public void initialize() {
-		pnlContainer.getChildren().clear();
-		mnuAlumno.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-					IActivator activator = new AlumnosActivator();
-					pnlContainer.getChildren().setAll((Pane)activator.getPane());
-			}
-		});
-		
-		mnuCursos.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-					IActivator activator = new CursosActivator();
-					pnlContainer.getChildren().setAll((Pane)activator.getPane());
-			}
-		});
-		
-		mnuAsignaturas.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-					IActivator activator = new AsignaturasActivator();
-					pnlContainer.getChildren().setAll((Pane)activator.getPane());
-			}
-		});
-		
-		mnuProfesores.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-					IActivator activator = new ProfesoresActivator();
-					pnlContainer.getChildren().setAll((Pane)activator.getPane());
-			}
-		});
-		
-		mnuColegios.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-					IActivator activator = new ColegiosActivator();
-					pnlContainer.getChildren().setAll((Pane)activator.getPane());
-			}
-		});
-		
-		mnuHabilidades.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-					IActivator activator = new HabilidadesActivator();
-					pnlContainer.getChildren().setAll((Pane)activator.getPane());
-			}
-		});
-		
-		mnuEjesTematicos.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-					IActivator activator = new EjesTematicosActivator();
-					pnlContainer.getChildren().setAll((Pane)activator.getPane());
-			}
-		});
-		
-		mnuHacerPrueba.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-					IActivator activator = new PruebasActivator();
-					pnlContainer.getChildren().setAll((Pane)activator.getPane());
-			}
-		});
-		mnuEvaluarPrueba.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-					IActivator activator = new EvaluacionPruebaActivator();
-					pnlContainer.getChildren().setAll((Pane)activator.getPane());
-			}
-		});
-		mnuItemGeneraBD.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent arg0) {
-				PersistenceServiceFactory.getPersistenceService();
-			}
-		});
-	}
+  @FXML
+  public void initialize() {
+    try {
+      WindowManager.getInstance().setRoot(pnlContainer);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    pnlContainer.getChildren().clear();
+    mnuAlumno.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent arg0) {
+        IActivator activator = new AlumnosActivator();
+        WindowManager.getInstance().show(activator.getPane(), true);
+      }
+    });
+
+    mnuCursos.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent arg0) {
+        IActivator activator = new CursosActivator();
+        WindowManager.getInstance().show(activator.getPane(), true);
+      }
+    });
+
+    mnuAsignaturas.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent arg0) {
+        IActivator activator = new AsignaturasActivator();
+        WindowManager.getInstance().show(activator.getPane(), true);
+      }
+    });
+
+    mnuProfesores.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent arg0) {
+        IActivator activator = new ProfesoresActivator();
+        WindowManager.getInstance().show(activator.getPane(), true);
+      }
+    });
+
+    mnuColegios.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent arg0) {
+        IActivator activator = new ColegiosActivator();
+        WindowManager.getInstance().show(activator.getPane(), true);
+      }
+    });
+
+    mnuHabilidades.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent arg0) {
+        IActivator activator = new HabilidadesActivator();
+        WindowManager.getInstance().show(activator.getPane(), true);
+      }
+    });
+
+    mnuEjesTematicos.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent arg0) {
+        IActivator activator = new EjesTematicosActivator();
+        WindowManager.getInstance().show(activator.getPane(), true);
+      }
+    });
+
+    mnuHacerPrueba.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent arg0) {
+        IActivator activator = new PruebasActivator();
+        WindowManager.getInstance().show(activator.getPane(), true);
+      }
+    });
+    mnuEvaluarPrueba.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent arg0) {
+        IActivator activator = new EvaluacionPruebaActivator();
+        WindowManager.getInstance().show(activator.getPane(), true);
+      }
+    });
+    mnuItemGeneraBD.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent arg0) {
+        PersistenceServiceFactory.getPersistenceService();
+      }
+    });
+  }
 }
--- kernel.api/src/cl/eos/interfaces/view/IWindowManager.java
+++ kernel.api/src/cl/eos/interfaces/view/IWindowManager.java
@@ -1,30 +1,52 @@
 package cl.eos.interfaces.view;
 
-import javafx.scene.Parent;
 
 public interface IWindowManager {
 
-	/**
-	 * Agrega una ventana al visualizador
-	 * @param window la ventana que se va a agregar.
-	 */
-	void show(final Parent window );
-	
-	/**
-	 * Saca una ventana de la visualización. 
-	 * @param window la ventana que se quiere sacar.
-	 */
-	void hide(final Parent window);
-	
-	/**
-	 * Cierra todas las ventanas.
-	 */
-	void hideAll();
+  /**
+   * Obtiene el elemento raiz principal que se gestiona.
+   * @return E
+   */
+  Object getRoot();
 
-	/**
-	 * Muestra una ventana con la opción de cerrar el resto.
-	 * @param window  Ventana a presentar.
-	 * @param closeOthers Verdadero si quiere cerrar el resto de ventanas.
-	 */
-	void show(final Parent window, boolean closeOthers);
+  void setRoot(final Object root) throws Exception;
+
+  /**
+   * Agrega una ventana al visualizador
+   * 
+   * @param window la ventana que se va a agregar.
+   */
+  void show(final Object window);
+
+  /**
+   * Saca una ventana de la visualizaciï¿½n.
+   * 
+   * @param window la ventana que se quiere sacar.
+   */
+  void hide(final Object window);
+
+  /**
+   * Cierra todas las ventanas.
+   */
+  void hideAll();
+
+  /**
+   * Muestra una ventana con la opciï¿½n de cerrar el resto.
+   * 
+   * @param window Ventana a presentar.
+   * @param closeOthers Verdadero si quiere cerrar el resto de ventanas.
+   */
+  void show(final Object window, boolean closeOthers);
+
+  /**
+   * Muestra window como hija de parent.
+   * 
+   * @param parent Contenedor de despliegue de window.
+   * @param widnow Quien se quiere mostrar.
+   */
+  void show(final Object parent, final Object widnow);
+
+  void show(final Object parent, final Object widnow, final boolean closeOthers);
+
+
 }
