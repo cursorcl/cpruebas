53fc0bc2943d6a0b9db521bfb66d914c905d445f
Eliecer Osorio Verdugo <cursor.cl@gmail.com>
2014-10-10 20:45:24 -0300

    exito con la rotacion

--- cpruebas.detection/src/cl/eos/detection/boofcv/ExampleBinaryOps.java
+++ cpruebas.detection/src/cl/eos/detection/boofcv/ExampleBinaryOps.java
@@ -7,12 +7,16 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
 import javax.imageio.ImageIO;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.ExplicitGroup;
 
 import boofcv.abst.feature.detect.line.DetectLineSegmentsGridRansac;
 import boofcv.alg.distort.DistortImageOps;
@@ -40,17 +44,29 @@ import boofcv.struct.image.MultiSpectral;
  * morphological operations, and then extracting detected features by finding
  * their contours.
  *
+ *  Imagen escaneada en una resolucion de 300dpi.
+ *  1)
+ *  1.l) El primer rectangulo oscuro está en 58,1541
+ *  1.2) La primera columna de circulo a leer está en 290, 1528 y la segunda en 710, 1528
+ *  1.3) Los circulos tienen una dimension de 44 pixeles
+ *  1.4) La separacion entre ciruclos de un bloque es 6 pixeles.
+ *  
+ *  2)
+ *  2.1) 58,1541 (+335)
+ *  2.2) 58,1876 (+334)
+ *  2.3) 58,2210 (+334)
+ *  2.4) 58,2544 (+333)
+ *  2.5) 58,2877
+ *
  * @author Peter Abeles
  */
 public class ExampleBinaryOps {
 
 	public static void main(String args[]) {
-		for (int m = 1; m < 5; m++) {
+		for (int m = 5; m < 6; m++) {
 			BufferedImage limage = UtilImageIO.loadImage("./res/prueba_00" + m
 					+ ".png");
 			
-			float  cx = limage.getWidth() / 2f;
-			float cy = limage.getHeight() / 2f;
 			BufferedImage image = limage.getSubimage(0, 1450, 150, 1700);
 
 			ImageFloat32 input = ConvertBufferedImage.convertFromSingle(image,
@@ -87,7 +103,8 @@ public class ExampleBinaryOps {
 			int dx = x[n - 1] - x[0];
 			int dy = y[n - 1] - y[0];
 
-			double radian = Math.atan2(dy, dx);
+			double radian = Math.PI / 2f - Math.atan2(dy, dx);
+//			double radian = 2 * Math.PI + (Math.PI / 2f - Math.atan2(dy, dx));
 
 			System.out.println("Angulo=" + radian);
 			int colorExternal = 0xFFFFFF;
@@ -104,19 +121,40 @@ public class ExampleBinaryOps {
 			try {
 				ImageIO.write(visualContour, "png", new File("./res/output" + m
 						+ ".png"));
+	            BufferedImage img = ExampleBinaryOps.rotate(radian, limage);
+	            ImageIO.write(img, "png", new File("./res/rotada" + m +".png"));
+
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 			
-			
-			MultiSpectral<ImageFloat32> distLeft =
-					ConvertBufferedImage.convertFromMulti(limage, null,true, ImageFloat32.class);
-			
-			MultiSpectral<ImageFloat32> rectLeft = new MultiSpectral<ImageFloat32>(ImageFloat32.class,
-					distLeft.getWidth(),distLeft.getHeight(),distLeft.getNumBands());
-			DistortImageOps.rotate(distLeft, rectLeft, boofcv.alg.interpolate.TypeInterpolate.BICUBIC, radian);
+
+//			ShowImages.showWindow(img, "Rotada");
 			
 		}
 		// ShowImages.showWindow(visualContour, "Contours");
 	}
+	
+	   public static BufferedImage rotate(double angle, BufferedImage image) {
+	        int drawLocationX = 0;
+	        int drawLocationY = 0;
+
+	        // Valores para la rotación.
+	        double rotationRequired = angle;
+	        double locationX = image.getWidth() / 2;
+	        double locationY = image.getHeight() / 2;
+	        AffineTransform tx = AffineTransform.getRotateInstance(
+	                rotationRequired, locationX, locationY);
+	        AffineTransformOp op = new AffineTransformOp(tx,
+	                AffineTransformOp.TYPE_BICUBIC);
+	        BufferedImage result = new BufferedImage(image.getWidth(),
+	                image.getHeight(), image.getType());
+	        Graphics2D g2d = (Graphics2D) result.getGraphics();
+
+	        // Se dibuja la imagen rotada.
+	        g2d.drawImage(op.filter(image, null), drawLocationX, drawLocationY,
+	                null);
+	        return result;
+	    }
+	
 }
