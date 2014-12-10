f05072497e2bb020ec4b9e58af84a2d13a590bee
Eliecer Osorio Verdugo <cursor.cl@gmail.com>
2014-12-10 00:26:42 -0300

    Actualización

--- cpruebas.persistence/src/cl/eos/persistence/IPersistenceService.java
+++ cpruebas.persistence/src/cl/eos/persistence/IPersistenceService.java
@@ -9,83 +9,76 @@ import cl.eos.interfaces.entity.IPersistenceListener;
 
 public interface IPersistenceService {
 
-	void save(IEntity entity);
+  void save(IEntity entity);
 
-	void update(IEntity entity);
+  void update(IEntity entity);
 
-	void delete(IEntity entity);
+  void delete(IEntity entity);
 
-	/**
-	 * Se utiliza principalmente para ejecutar operaciones de actualizacion en
-	 * la base de datos (DELETE, UPDATE MASIVO etc.)
-	 * 
-	 * @param namedQuery
-	 *            Query nombrado asociado a una entidad.
-	 * @param parameters
-	 *            Los parametros que requiere el query.
-	 * @return Numero de filas afectadas.
-	 */
-	int executeUpdate(final String namedQuery, Map<String, Object> parameters);
+  /**
+   * Se utiliza principalmente para ejecutar operaciones de actualizacion en la base de datos
+   * (DELETE, UPDATE MASIVO etc.)
+   * 
+   * @param namedQuery Query nombrado asociado a una entidad.
+   * @param parameters Los parametros que requiere el query.
+   * @return Numero de filas afectadas.
+   */
+  int executeUpdate(final String namedQuery, Map<String, Object> parameters);
 
-	/**
-	 * Busca todos los registros asociados al entity mencionado.
-	 * 
-	 * @param entityClazz
-	 *            Entidad de la que se requieren todos los registros.
-	 * @param listener
-	 *            A quien se notifica del resultado.
-	 */
-	void findAll(Class<? extends IEntity> entityClazz,
-			final IPersistenceListener listener);
+  /**
+   * Busca todos los registros asociados al entity mencionado.
+   * 
+   * @param entityClazz Entidad de la que se requieren todos los registros.
+   * @param listener A quien se notifica del resultado.
+   */
+  void findAll(Class<? extends IEntity> entityClazz, final IPersistenceListener listener);
 
-	/**
-	 * Busca la entidad asociada con el identificador.
-	 * 
-	 * @param entityClazz
-	 *            Entidad de la que se requieren la b�squeda.
-	 * @param listener
-	 *            A quien se notifica del resultado.
-	 */
-	void findById(Class<? extends IEntity> entityClazz, Long id,
-			final IPersistenceListener listener);
+  /**
+   * Busca todos los registros asociados al entity mencionado.
+   * La busqueda es sincrónica, por tanto el llamante queda detenido hasta terminar de procesar la consulta.
+   * @param entityClazz Entidad de la que se requieren todos los registros. * @return
+   * return Lista de valores encontrados.
+   */
+  List<Object> findAllSynchro(final Class<? extends IEntity> entityClazz);
 
-	/**
-	 * Busca la entidad asociada con el identificador.
-	 * 
-	 * @param entityClazz
-	 *            Entidad de la que se requieren la b�squeda.
-	 * @param listener
-	 *            A quien se notifica del resultado.
-	 */
-	void findByAllId(Class<? extends IEntity> entityClazz, Object[] id,
-			final IPersistenceListener listener);
+  /**
+   * Busca la entidad asociada con el identificador.
+   * 
+   * @param entityClazz Entidad de la que se requieren la b�squeda.
+   * @param listener A quien se notifica del resultado.
+   */
+  void findById(Class<? extends IEntity> entityClazz, Long id, final IPersistenceListener listener);
 
-	/**
-	 * Busca la entidad asociada con el nombre.
-	 * 
-	 * @param entityClazz
-	 *            Entidad de la que se requieren la b�squeda.
-	 * @param listener
-	 *            A quien se notifica del resultado.
-	 */
-	void findByName(Class<? extends IEntity> entityClazz, String name,
-			final IPersistenceListener listener);
+  /**
+   * Busca la entidad asociada con el identificador.
+   * 
+   * @param entityClazz Entidad de la que se requieren la b�squeda.
+   * @param listener A quien se notifica del resultado.
+   */
+  void findByAllId(Class<? extends IEntity> entityClazz, Object[] id,
+      final IPersistenceListener listener);
 
-	/**
-	 * Ejecuta una consulta en base a una consulta nombrada.
-	 * 
-	 * @param namedQuery
-	 *            Nombre del query que se ejecuta.
-	 * @param parameters
-	 *            parametros para la consulta.
-	 * @param listener
-	 *            A quien se notifica del resultado.
-	 */
-	void find(final String namedQuery, final Map<String, Object> parameters,
-			final IPersistenceListener listener);
+  /**
+   * Busca la entidad asociada con el nombre.
+   * 
+   * @param entityClazz Entidad de la que se requieren la b�squeda.
+   * @param listener A quien se notifica del resultado.
+   */
+  void findByName(Class<? extends IEntity> entityClazz, String name,
+      final IPersistenceListener listener);
 
-	void disconnect();
+  /**
+   * Ejecuta una consulta en base a una consulta nombrada.
+   * 
+   * @param namedQuery Nombre del query que se ejecuta.
+   * @param parameters parametros para la consulta.
+   * @param listener A quien se notifica del resultado.
+   */
+  void find(final String namedQuery, final Map<String, Object> parameters,
+      final IPersistenceListener listener);
 
-	void insert(String entity, List<Object> list, IPersistenceListener listener) throws ExceptionBD;
+  void disconnect();
+
+  void insert(String entity, List<Object> list, IPersistenceListener listener) throws ExceptionBD;
 
 }
